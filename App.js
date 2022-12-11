a789690dd0fa9992409ba387ce6fa36ddc1fcd8c
Mehtap Tataroğlu <teytap@gmail.com>
2022-12-11 08:31:55 +0300

    added notificationNumber to asRead, made responsive

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 const notificationEl = document.querySelectorAll(".notification");
+const notificationNumber = document.getElementById("notification-number");
 const redRecord = document.querySelectorAll(".red-record");
 console.log(notificationEl);
 function asRead() {
+  notificationNumber.textContent = "0";
   for (let i = 0; i < notificationEl.length; i++) {
     notificationEl[i].classList.remove("recent");
     redRecord[i].style.display = "none";
